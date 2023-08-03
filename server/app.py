8845f64e1ac9ea01c535465c7c6c1d46cbe92233
Christian Garcia <bones192nd@gmail.com>
2023-08-03 14:50:09 -0400

    didn't save all before committing

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# app.py
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 # server/models.py
+# models.py
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
